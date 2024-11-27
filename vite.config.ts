75c088b52ecc2175695dd733c9840096937fa28c
middlewarefe <70000353+middlewarefe@users.noreply.github.com>
2024-11-27 15:06:11 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ import tailwindcss from 'tailwindcss'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    host: true, // 设置为 true 以使服务器在本地网络上可访问
+    port: 30000, // 可选：设置端口号，默认为 3000
+  },
   css: {
     postcss: {
       plugins: [tailwindcss()],
